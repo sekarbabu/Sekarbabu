81ebef39ce351564414f4d7ba125daed84515955
Your Name <you@example.com>
2019-12-12 14:18:32 -0700

    Challenge3

--- challenge3.py
+++ challenge3.py
@@ -1,6 +1,6 @@
 import unittest
 from selenium import webdriver
-from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
 
 
 class Challenge3(unittest.TestCase):
@@ -11,35 +11,11 @@ class Challenge3(unittest.TestCase):
     def tearDown(self):
         self.driver.close()
 
-    def test_challege3(self):
+    def test_challege3forLoop(self):
         self.driver.get("https://www.copart.com")
-
-    def test_challenge3forLoop(self):
-        Cars = ["ALTIMA", "SILVERADO", "TOYOTA HYUNDAI", "SONATA", "SENTRA", "FORD", "VOLVO", "COROLLA", "CHARGER",
-                "HONDA",
-                "BMW", "CAMRY", "ODYSSEY EX", "NISSAN",
-                "DODGE", "ACCORD EX", "F150 SUPER", "CHEVROLET", "KIA"]
-        for temp in Cars:
-            self.driver.find_element_by_xpath("//table[@id='serverSideDataTable']")
-            print temp
-
-
-     # self.driver.find_element_by_xpath("//div[@id='tabTrending']")
-
-
-
-
-
-    #  // *[ @ id = "tabTrending"] / div[1] / div[2] / div[2] / ul / li[3] / a
-
-    # def test_challenge3whileLoop(self):
-    # Cars = ["ALTIMA", "SILVERADO", "TOYOTA HYUNDAI", "SONATA", "SENTRA", "FORD", "VOLVO", "COROLLA", "CHARGER",
-    #        "HONDA", "BMW", "CAMRY", "ODYSSEY EX", "NISSAN",
-    #        "DODGE", "ACCORD EX", " F150 SUPER", "CHEVROLET", "KIA"]
-    # print Cars
-
-    # self.driver.get("https://www.copart.com")
-
+        Models = "//*[@ng-if='popularSearches']//a"
+        for a in self.driver.find_elements(By.XPATH, Models):
+            print(a.text + " - " + a.get_attribute('href'))
 
 if __name__ == '__main__':
     unittest.main()
