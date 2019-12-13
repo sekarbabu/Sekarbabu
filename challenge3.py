60e2e562c631609644ba5d1bd1aa16d9f162d27b
Your Name <you@example.com>
2019-12-12 20:09:33 -0700

    no message

--- challenge3.py
+++ challenge3.py
@@ -7,16 +7,23 @@ class Challenge3(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome("./chromedriver")
-
+        self.driver.get("https://www.copart.com")
+        self.assertIn("Copart", self.driver.title)
 
     def tearDown(self):
         self.driver.close()
 
     def test_challege3forLoop(self):
-        self.driver.get("https://www.copart.com")
-        self.assertIn("Copart", self.driver.title)
         for a in self.driver.find_elements(By.XPATH, "//*[@ng-if='popularSearches']//a"):
             print(a.text + " - " + a.get_attribute('href'))
+
+    def test_challege3whileLoop(self):
+        elements = self.driver.find_elements(By.XPATH, '//*[@id="tabTrending"]/div[3]//a')
+        # print(len(elements))
+        i = 0
+        while i < len(elements):
+            print (elements[i].text + ": " + elements[i].get_attribute("href"))
+            i += 1
 
 
 if __name__ == '__main__':
